5b83f936293f267fd678bcbcb158cabe1edb51e4
Brittany Miller <bmiller012712@gmail.com>
2020-08-04 08:31:33 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lesson1_3_ASPNETMVC_FirstWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
